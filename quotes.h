5be4d268c92579c541ee0e5ad29c7edfcd536fba
nass11m <nassim.el-bennane@efrei.net>
2025-03-28 18:02:39 +0100

    Initial quotes program

---
+++ quotes.h
@@ -0,0 +1,16 @@
+//
+// Created by nasse on 28/03/2025.
+//
+#ifndef QUOTES_H
+#define QUOTES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_QUOTES 10
+
+void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+#endif
